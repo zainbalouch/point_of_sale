5270580fb370e07f4b901195fcbf067d08f4d54b
zainbalouch <zainuleman786@gmail.com>
2025-05-11 12:36:55 +0300

    testing to print on mobile

--- resources/views/components/print-script.blade.php
+++ resources/views/components/print-script.blade.php
@@ -1,21 +1,32 @@
 @once
     <script>
         function openPrintPreview(url) {
-            // Remove any existing print iframe
-            const existingIframe = document.querySelector('iframe[style*="visibility: hidden"]');
-            if (existingIframe) {
-                existingIframe.remove();
+            // Check if it's a mobile device
+            const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+            if (isMobile) {
+                // For mobile devices, open in new window and print
+                const printWindow = window.open(url, '_blank');
+                printWindow.onload = function() {
+                    printWindow.print();
+                };
+            } else {
+                // For desktop browsers, use iframe approach
+                const existingIframe = document.querySelector('iframe[style*="visibility: hidden"]');
+                if (existingIframe) {
+                    existingIframe.remove();
+                }
+
+                var iframe = document.createElement('iframe');
+                iframe.style.visibility = 'hidden';
+                document.body.appendChild(iframe);
+
+                iframe.src = url;
+
+                iframe.onload = function() {
+                    iframe.contentWindow.print();
+                };
             }
-
-            var iframe = document.createElement('iframe');
-            iframe.style.visibility = 'hidden';
-            document.body.appendChild(iframe);
-
-            iframe.src = url;
-
-            iframe.onload = function() {
-                iframe.contentWindow.print();
-            };
         }
     </script>
     @if (session()->has('created_invoice_id'))
